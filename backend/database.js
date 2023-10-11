d4d0d4ae2375992dfc61ac0617b92881593788c1
yicheng-toh <tohyicheng1989@gmail.com>
2023-10-11 22:21:42 +0800

    Create SQL tables inaccordance with Week 4 proposal

--- backend/database.js
+++ backend/database.js
@@ -1,37 +1,67 @@
-const mysql = require('mysql2');
-const dbDetails = require('./yc_data');
-
+const mysql = require("mysql2");
+const dbDetails = require("./yc_data");
 
 const dbConnection = mysql.createConnection({
-    host: dbDetails.host,
-    user: dbDetails["user"],
-    password: dbDetails.password,
-    database:  dbDetails.database,
-
-})
+  host: dbDetails.host,
+  user: dbDetails["user"],
+  password: dbDetails.password,
+  database: dbDetails.database,
+});
 
 // Function to create the BASESENSOR table if it doesn't exist
 async function createTableIfNotExists() {
-    try {
-      const createTableQuery = `
-        CREATE TABLE IF NOT EXISTS BASESENSOR (
-          id INT AUTO_INCREMENT PRIMARY KEY,
-          temperature FLOAT,
-          humidity FLOAT
+  try {
+    //   const createTableQuery = `
+    //     CREATE TABLE IF NOT EXISTS BASESENSOR (
+    //       id INT AUTO_INCREMENT PRIMARY KEY,
+    //       temperature FLOAT,
+    //       humidity FLOAT
+    //     )
+    //   `;
+    // Create the Sensor Detail table
+    const createSensorDetailTable = `
+        CREATE TABLE SensorDetail (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        dateTime DATETIME,
+        microcontrollerId INT,
+        batchNo INT,
+        brightness INT,
+        temperature FLOAT,
+        humidity INT
         )
-      `;
-      await dbConnection.execute(createTableQuery);
-      console.log("Table BASESENSOR created or already exists.");
-    } catch (error) {
-      console.error("Error creating table:", error);
-    }
+        `;
+
+    // Create the Microcontroller Location table
+    const createMicrocontrollerLocationTable = `
+        CREATE TABLE MicrocontrollerLocation (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        microcontrollerId INT,
+        position INT
+        )
+        `;
+
+    // Create the Plant Batch table
+    const createPlantBatchTable = `
+        CREATE TABLE PlantBatch (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        batchNo INT,
+        plantSpecies VARCHAR(255),
+        position INT
+        )
+        `;
+    await dbConnection.execute(createSensorDetailTable);
+    await dbConnection.execute(createMicrocontrollerLocationTable);
+    await dbConnection.execute(createPlantBatchTable);
+    console.log("Tables created or already exists.");
+  } catch (error) {
+    console.error("Error creating table:", error);
   }
-  
-  // Call the function to create the table before running the server
+}
+
+// Call the function to create the table before running the server
 //   createTableIfNotExists();
 
-
 module.exports = {
-    dbConnection,
-    createTableIfNotExists,
+  dbConnection,
+  createTableIfNotExists,
 };
