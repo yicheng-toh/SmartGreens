2e5247f9a079b9eeec9cda8b3d160bc0917f8acc
DESKTOP-ITCTSNF\YiCheng <tohyicheng1989@gmail.com>
2024-01-13 12:26:21 +0800

    Update inventory route and inventory function

--- backend/src/database_logic/sql/mysql/inventory_mysql.js
+++ backend/src/database_logic/sql/mysql/inventory_mysql.js
@@ -1,45 +1,47 @@
 
 
-//inventory table
 const {dbConnection} = require("../mysql.js");
+
 //insertNewInventoryObject
 async function insertNewInventoryObject(itemName, quantity, units){
-    if(quantity == NULL){
-        quantity = 0;
-    }
     await dbConnection.execute('INSERT INTO Inventory (InventoryName,Quantity,Units) VALUES (?, ?, ?)',
         [itemName, quantity, units]);
     return 1;
 }
 
+//verifyInventoryIdExist
+async function verifyInventoryIdExist(inventoryId){
+    const inventoryIdList = await dbConnection.execute('SELECT id FROM Inventory WHERE InventoryName = ?', itemName);
+    return inventoryIdList.length;
+}
 
 //updateInventoryQuantity TODO!!!paused here.
-async function updateInventoryQuantity(itemName, quantityChange){
-    //check if the itemName exists.
-    //update the quantity
-    const inventoryIdList = await dbConnection.execute('SELECT id FROM Inventory WHERE InventoryName = ?', itemName);
-    if(inventoryIdList.length == 0){
-        return 0;
-    }
-    const inventoryId = invetoryIdList[0];
+async function updateInventoryQuantity(inventoryId, quantityChange){
     const currentQuantity = await dbConnection.execute('SELECT quantity FROM Inventory WHERE InventoryID = ?', inventoryId);
     const newQuantity = currentQuantity + quantityChange;
     await dbConnection.execute('UPDATE Inventory SET quantity = ? WHERE InventoryID = ?;', [newQuantity, inventoryId]);
     return 1;
 }
 //updateInventoryUnits
-async function updateInventoryUnit(itemName, newUnit){
-    //check if the itemName exists.
-    //update the quantity
-    const inventoryIdList = await dbConnection.execute('SELECT id FROM Inventory WHERE InventoryName = ?', itemName);
-    if(inventoryIdList.length == 0){
-        return 0;
-    }
-    const inventoryId = inventoryIdList[0];
+async function updateInventoryUnit(inventoryId, newUnit){
     await dbConnection.execute('UPDATE Inventory SET Units = ? WHERE InventoryID = ?;', [newUnit, inventoryId]);
     return 1;
 }
 //deleteInventoryObject
-async function deleteInventoryObject(itemName){
-    await dbConnection.execute('DELETE FROM Inventory WHERE itemName = ?', [itemName]);
+async function deleteInventoryObject(inventoryId){
+    await dbConnection.execute('DELETE FROM Inventory WHERE InventoryID = ?', [inventoryId]);
+}
+
+//getAllInventoryData
+async function getAllInventoryData(){
+    return await dbConnection.execute('SELECT * FROM Inventory');
+}
+
+module.exports ={
+    insertNewInventoryObject,
+    verifyInventoryIdExist,
+    updateInventoryQuantity,
+    updateInventoryUnit,
+    deleteInventoryObject,
+    getAllInventoryData,
 }
