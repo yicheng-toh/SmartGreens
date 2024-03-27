9c3f7bacd653a3d8fbb5cc17a76031708ae0ba5d
DESKTOP-ITCTSNF\YiCheng <tohyicheng1989@gmail.com>
2024-03-27 20:16:19 +0800

    Add try catch and connection disconnect for mssql

--- backend/src/database_logic/sql/mssql/energy_consumption_mssql.js
+++ backend/src/database_logic/sql/mssql/energy_consumption_mssql.js
@@ -11,10 +11,11 @@ async function insertNewEnergyConsumingDevice(deviceName, quantity, energyConsum
     .input('quantity', sql.Int, quantity)
     .input('energyConsumption', sql.Float, energyConsumption)
     .query('INSERT INTO EnergyConsumingDevice (DeviceName, Quantity, EnergyConsumption) VALUES (@deviceName, @quantity, @energyConsumption)');
-
+    await dbConnection.disconnect();
     return 1; // Return success
   } catch (error) {
     console.error("Error inserting new energy consuming device:", error.message);
+    await dbConnection.disconnect();
     throw error;
   }
 }
@@ -27,9 +28,10 @@ async function verifyEnergyConsumingDeviceIdExist(deviceId) {
     const result = await request
     .input('deviceId', sql.Int, deviceId)
     .query("SELECT * FROM EnergyConsumingDevice WHERE DeviceId = @deviceId");
-
+    await dbConnection.disconnect();
     return result.recordset.length;
   } catch (error) {
+    await dbConnection.disconnect();
     console.error("Error verifying energy consuming device existence:", error.message);
     throw error;
   }
@@ -48,8 +50,10 @@ async function updateEnergyConsumingDevice(deviceId, newEnergyDeviceName, newQua
     .query(
       "UPDATE EnergyConsumingDevice SET DeviceName = @newEnergyDeviceName, Quantity = @newQuantity, EnergyConsumption = @newEnergyConsumption WHERE DeviceId = @deviceId"
     );
+    await dbConnection.disconnect();
     return 1; // Return success
   } catch (error) {
+    await dbConnection.disconnect();
     console.error("Error updating energy consuming device:", error.message);
     throw error;
   }
@@ -63,9 +67,10 @@ async function deleteEnergyConsumingDevice(deviceId) {
     await request
     .input('deviceId', sql.Int, deviceId)
     .query("DELETE FROM EnergyConsumingDevice WHERE DeviceId = @deviceId");
-
+    await dbConnection.disconnect();
     return 1; // Return success
   } catch (error) {
+    await dbConnection.disconnect();
     console.error("Error deleting energy consuming device:", error.message);
     throw error;
   }
@@ -77,8 +82,10 @@ async function getAllEnergyConsumingDevice() {
     const request = await dbConnection.connect();
   try {
     const result = await request.query("SELECT * FROM EnergyConsumingDevice");
+    await dbConnection.disconnect();
     return result.recordset;
   } catch (error) {
+    await dbConnection.disconnect();
     console.error("Error fetching all energy consuming devices:", error.message);
     throw error;
   }
@@ -90,8 +97,10 @@ async function getTotalEnergyConsumptionValue() {
     const request = await dbConnection.connect();
   try {
     const result = await request.query(`SELECT SUM(COALESCE(Quantity, 0) * COALESCE(EnergyConsumption, 0)) AS EnergyUsage FROM EnergyConsumingDevice`);
+    await dbConnection.disconnect();
     return result.recordset[0];
   } catch (error) {
+    await dbConnection.disconnect();
     console.error("Error fetching total energy consumption value:", error.message);
     throw error;
   }
--- backend/src/database_logic/sql/mysql/energy_consumption_mysql.js
+++ backend/src/database_logic/sql/mysql/energy_consumption_mysql.js
@@ -6,20 +6,39 @@ async function insertNewEnergyConsumingDevice(
   quantity,
   energyConsumption
 ) {
-  await dbConnection.execute(
-    "INSERT INTO EnergyConsumingDevice (DeviceName, Quantity, EnergyConsumption) VALUES ( ?, ?, ?)",
-    [deviceName, quantity, energyConsumption]
-  );
-  return 1;
+  try {
+    await dbConnection.execute(
+      "INSERT INTO EnergyConsumingDevice (DeviceName, Quantity, EnergyConsumption) VALUES ( ?, ?, ?)",
+      [deviceName, quantity, energyConsumption]
+    );
+    return 1;
+  } catch (error) {
+    console.error(
+      "Error inserting new energy consuming device:",
+      error.message
+    );
+    throw error;
+  }
 }
 
 //verifyEnergyConsumingDeviceExist
 async function verifyEnergyConsumingDeviceIdExist(deviceId) {
-  const EnergyConsumingDeviceIdList = await dbConnection
-    .promise()
-    .query("SELECT * FROM EnergyConsumingDevice WHERE DeviceId = ?", deviceId);
-  // console.log(EnergyConsumingDeviceIdList);
-  return EnergyConsumingDeviceIdList[0].length;
+  try {
+    const EnergyConsumingDeviceIdList = await dbConnection
+      .promise()
+      .query(
+        "SELECT * FROM EnergyConsumingDevice WHERE DeviceId = ?",
+        deviceId
+      );
+    // console.log(EnergyConsumingDeviceIdList);
+    return EnergyConsumingDeviceIdList[0].length;
+  } catch (error) {
+    console.error(
+      "Error verifying energy consuming device existence:",
+      error.message
+    );
+    throw error;
+  }
 }
 
 //updateEnergyConsumingDeviceQuantity TODO!!!paused here.
@@ -29,35 +48,62 @@ async function updateEnergyConsumingDevice(
   newQuantity,
   newEnergyConsumption
 ) {
-  await dbConnection.execute(
-    "UPDATE EnergyConsumingDevice SET DeviceName = ?, Quantity = ?, EnergyConsumption = ? WHERE DeviceId = ?;",
-    [newEnergyDeviceName, newQuantity, newEnergyConsumption, deviceId]
-  );
-  return 1;
+  try {
+    await dbConnection.execute(
+      "UPDATE EnergyConsumingDevice SET DeviceName = ?, Quantity = ?, EnergyConsumption = ? WHERE DeviceId = ?;",
+      [newEnergyDeviceName, newQuantity, newEnergyConsumption, deviceId]
+    );
+    return 1;
+  } catch (error) {
+    console.error("Error updating energy consuming device:", error.message);
+    throw error;
+  }
 }
 
 //deleteEnergyConsumingDeviceObject
 async function deleteEnergyConsumingDevice(deviceId) {
-  await dbConnection.execute("DELETE FROM EnergyConsumingDevice WHERE DeviceId = ?", [
-    deviceId,
-  ]);
-  return 1;
+  try {
+    await dbConnection.execute(
+      "DELETE FROM EnergyConsumingDevice WHERE DeviceId = ?",
+      [deviceId]
+    );
+    return 1;
+  } catch (error) {
+    console.error("Error deleting energy consuming device:", error.message);
+    throw error;
+  }
 }
 
 //getAllEnergyConsumingDeviceData
 async function getAllEnergyConsumingDevice() {
-  const result = await dbConnection.promise().query("SELECT * FROM EnergyConsumingDevice");
-  return result[0][0];
+  try {
+    const result = await dbConnection
+      .promise()
+      .query("SELECT * FROM EnergyConsumingDevice");
+    return result[0][0];
+  } catch (error) {
+    console.log("Error fetching all energy consumption device:", error);
+    throw error;
+  }
 }
 
-async function getTotalEnergyConsumptionValue(){
-    const result = await dbConnection.promise().query(`SELECT SUM(COALESCE(Quantity, 0) * COALESCE(EnergyConsumption, 0)) AS EnergyUsage
+async function getTotalEnergyConsumptionValue() {
+  try {
+    const result = await dbConnection.promise()
+      .query(`SELECT SUM(COALESCE(Quantity, 0) * COALESCE(EnergyConsumption, 0)) AS EnergyUsage
     FROM EnergyConsumingDevice;
     `);
-    console.log("getTotalEnergyConsumptionValue",result);
-    console.log("getTotalEnergyConsumptionValue",result[0]);
-    console.log("getTotalEnergyConsumptionValue",result[0][0]);
+    console.log("getTotalEnergyConsumptionValue", result);
+    console.log("getTotalEnergyConsumptionValue", result[0]);
+    console.log("getTotalEnergyConsumptionValue", result[0][0]);
     return result[0][0];
+  } catch (error) {
+    console.error(
+      "Error fetching all energy consuming devices:",
+      error.message
+    );
+    throw error;
+  }
 }
 
 module.exports = {
@@ -66,5 +112,5 @@ module.exports = {
   updateEnergyConsumingDevice,
   deleteEnergyConsumingDevice,
   getAllEnergyConsumingDevice,
-  getTotalEnergyConsumptionValue
+  getTotalEnergyConsumptionValue,
 };
